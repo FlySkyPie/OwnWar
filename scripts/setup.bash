a851258df0015b0f452be2fe6b608dd280ec35de
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2023-08-26 22:49:53 +0800

    feat: Update dockerfile
    Build and extract two native library

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -7,4 +7,8 @@ mkdir -p ${BUILD_DIR}
 
 docker build . -f ${SCRIPT_DIR}/../docker/Dockerfile -t ownwar-deps-builder:latest
 docker run --rm --entrypoint cat ownwar-deps-builder:latest \
-    /app/godot/bin/godot.x11.tools.64 >${BUILD_DIR}/Godot_v3.3.5-modified_x11.64
+    /build/Godot_v3.3.5-modified_x11.64 >${BUILD_DIR}/Godot_v3.3.5-modified_x11.64
+docker run --rm --entrypoint cat ownwar-deps-builder:latest \
+    /build/libgd_3d_batcher.so >${BUILD_DIR}/libgd_3d_batcher.so
+docker run --rm --entrypoint cat ownwar-deps-builder:latest \
+    /build/libgd_3d_batcher.so >${BUILD_DIR}/libhterrain.so
