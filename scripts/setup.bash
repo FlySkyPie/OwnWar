910af65c29f65b911efe22b1587937bee2ddbf1f
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2023-08-27 12:36:11 +0800

    feat: Update setup script
    - Add export template setup.

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -28,4 +28,28 @@ docker run --rm --entrypoint cat ownwar-deps-builder:latest \
 docker run --rm --entrypoint cat ownwar-deps-builder:latest \
     /build/libownwar.so >${BUILD_DIR}/libownwar.so
 
+echo "Install native libraries to the game project..."
 cp ${BUILD_DIR}/*.so ${GAME_PROJECT_DIR}/gd/lib/.
+
+if [ ! -f "${BUILD_DIR}/Godot_v3.3.4-stable_export_templates.tpz" ]; then
+    echo "Extracting Godot engine..."
+    docker run --rm --entrypoint cat ownwar-deps-builder:latest \
+        /build/Godot_v3.3.4-stable_export_templates.tpz >${BUILD_DIR}/Godot_v3.3.4-stable_export_templates.tpz
+else
+    echo "Godot export templates exist."
+fi
+
+TEMPLATE_DIR=${HOME}/.local/share/godot/templates/3.3.5.rc
+
+if [ ! -d "${TEMPLATE_DIR}" ]; then
+    echo "Installing export templates..."
+
+    mkdir -p ${TEMPLATE_DIR}
+
+    unzip -j ${BUILD_DIR}/Godot_v3.3.4-stable_export_templates.tpz \
+        templates/* \
+        -d ${TEMPLATE_DIR}
+
+else
+    echo "Export templates installed."
+fi
